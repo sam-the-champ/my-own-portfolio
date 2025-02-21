e184be96dc071d9ceebf6561d0f0eda37e9b02aa
Olalekan Ogundimu <ogundimuolalekan55@gmail.com>
2025-02-21 15:43:58 -0800

    updated the js file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ themeToggle.addEventListener("click", () => {
 
 // Resume Download
 function downloadResume() {
-    const resumeUrl = "https://docs.google.com/document/d/1F9WvV3NI8IFkQ7MHoEUKD6Xfz-G_IczI1r6f1NqkIWw/edit?usp=drivesdk"; // Replace with actual resume file
+    const resumeUrl = "https://drive.google.com/uc?export=download&id=1F9WvV3NI8IFkQ7MHoEUKD6Xfz-G_IczI1r6f1NqkLWw"; // Replace with actual resume file
     const link = document.createElement("a");
     link.href = resumeUrl;
     link.download = "Olalekan_Samson_Ogundimu_Resume.pdf";
